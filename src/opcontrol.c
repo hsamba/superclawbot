02234377257b2dd17ab7dd9c26ad956759ea106f
hsamba <helensamba@students.berkeley.net>
2021-10-22 15:24:23 -0700

    straight line 10/22

--- src/opcontrol.c
+++ src/opcontrol.c
@@ -11,66 +11,7 @@
 #include "chassis.h"
 #include "math.h"
 #include "arm.h"
-//*
-void homeshoulder(){
-  Encoder shoulderEncoder;
-	shoulderEncoder = encoderInit(SHOULDER_ENCODER_TOP_PORT, SHOULDER_ENCODER_LOWER_PORT, false);
-	Encoder elbowEncoder;
-	elbowEncoder = encoderInit(ELBOW_ENCODER_TOP_PORT, ELBOW_ENCODER_LOWER_PORT, false);
-  int elbowCounts;
-  int shoulderCounts;
-  while (joystickGetDigital(1, 7, JOY_UP) == 0 & joystickGetDigital(1, 7, JOY_DOWN) == 0){
-    wait(1);
-  }
-  if(joystickGetDigital(1, 7, JOY_UP) == 1){
-  shoulderMove(50);
-	elbowMove(-50);
-	delay(1000);
-	elbowMove(0);
-	while(digitalRead(SHOULDER_LIMIT_SWITCH) == HIGH){
-		shoulderMove(50);
-		printf("shoulder Enc %d\n", encoderGet(shoulderEncoder));
-		delay(100);
-	}
-	printf("UpperLimitSwitchisPressed \n");
-	encoderReset(shoulderEncoder);
-	while((encoderGet(shoulderEncoder))<125){
-		printf("shoulder encoder %d\n", encoderGet(shoulderEncoder));
-		shoulderMove(-165+(encoderGet(shoulderEncoder)));
-    delay(150);
-	}
-	encoderReset(shoulderEncoder);
-	shoulderMove(encoderGet(shoulderEncoder)-0);
-	while(digitalRead(ELBOW_LIMIT_SWITCH) == HIGH){
-		elbowMove(-50);
-    printf("elbow encoder %d\n", encoderGet(elbowEncoder));
-		shoulderMove(encoderGet(shoulderEncoder)-0);
-    delay(150);
-	}
-	encoderReset(elbowEncoder);
-  while((encoderGet(elbowEncoder))<75){
-		elbowMove(100-(encoderGet(elbowEncoder)));
-  }
-	 encoderReset(elbowEncoder);
-    while(joystickGetDigital(1, 8, JOY_DOWN) == 0){
-      shoulderMove(2*(0-encoderGet(shoulderEncoder)));
-      elbowMove(2*(0-encoderGet(elbowEncoder)));
-    }
-  } else if (joystickGetDigital(1, 7, JOY_DOWN) == 1) {
-  elbowCounts = encoderGet(elbowEncoder);
-  shoulderCounts = encoderGet(shoulderEncoder);
-  while(joystickGetDigital(1, 8, JOY_RIGHT) == 0){
-    shoulderMove(-4*(shoulderCounts-encoderGet(shoulderEncoder)));
-    elbowMove(4*(elbowCounts-encoderGet(elbowEncoder)));
-    printf("elbow %d\n", elbowCounts-encoderGet(elbowEncoder));
-    printf("shoulder %d\n", shoulderCounts-encoderGet(shoulderEncoder));
-    printf("shoulder enc %d\n", encoderGet(shoulderEncoder));
-    printf("elbow enc %d\n", encoderGet(elbowEncoder));
-    wait(1);
-  }
-}
-}
-//
+
 void operatorControl() {
   Encoder shoulderEncoder;
   shoulderEncoder = encoderInit(SHOULDER_ENCODER_TOP_PORT, SHOULDER_ENCODER_LOWER_PORT, false);
@@ -84,10 +25,17 @@ void operatorControl() {
 	int claw;
   int elbowCounts;
   int shoulderCounts;
-  int ShoulderLength;
-  int ElbowLength;
-  ShoulderLength = 22;
-  ElbowLength = 29;
+  int LShoul;
+  int LElb;
+  int targetX = 7;
+  int targetY = 0;
+  int targetS;
+  int targetE;
+  int errorS;
+  int errorE;
+  double D;
+  LShoul = 22;
+  LElb = 29;
 	while (1) {
     //CONTROLS
 		power = joystickGetAnalog(1, 1);
@@ -156,25 +104,31 @@ void operatorControl() {
     }
     if (joystickGetDigital(1, 7, JOY_LEFT) == 1){
       while(joystickGetDigital(1, 7, JOY_DOWN) == 0){
-      shoulderMove(shoulder);
-      elbowMove((encoderGet(shoulderEncoder)+asin(ShoulderLength*(sin(encoderGet(elbowEncoder)*PI/180))/ElbowLength))-encoderGet(elbowEncoder));
+        D = ((targetX)^2 + (targetY)^2 - (LShoul)^2 - (LElb)^2)/2*LShoul*LElb;
+        targetE = (180/3.1415)*atan(D*sqrt(1-D));
+        targetS = (180/3.1415)*(atan(targetY/targetX) - atan((LElb*sin(targetE))/(LShoul+cos(targetE))));
+        errorE = targetE - encoderGet(elbowEncoder);
+        errorS = targetS - encoderGet(shoulderEncoder);
+        elbowMove(4*errorE);
+        shoulderMove(4*errorS);
+        if(joystickGetDigital(1, 7, JOY_UP) == 1){
+          targetX = targetX + 1;
+          targetY = targetY + 1;
+        }
+  //  shoulderMove(shoulder);
+  //  elbowMove((encoderGet(shoulderEncoder)+asin(LShoul*(sin(encoderGet(elbowEncoder)*3.1415/180))/LElb))-encoderGet(elbowEncoder));
       }
     }
     //END STRAIGHT LINE
 	}
 }
 
-//* if (digitalRead(SHOULDER_LIMIT_SWITCH) == LOW){
-// printf("UpperLimitSwitchisPressed \n");
-// printf("%d\n", encoderGet(shoulderEncoder));
-// encoderReset(shoulderEncoder);
 /*
-
 int l1
 int l2
 
-int targetX = 0;
-int targetY = 18;
+int targetX = 7;
+int targetY = 0;
 int targetS :
 int targetE
 int errorS ;
@@ -187,14 +141,14 @@ double D;
 
 //use targetX and targetY to calculate targetE and targetS
 D = ((targetX)^2 + (targetY)^2 - (l1)^2 - (l2)^2)/2*l1*l2
-targetE = atan2(sqtr(1-D)D )
-targetS = atan2(targetY/targetX) - atan2((L2*sin(targetE))/(L1+cos(targetE)))
+targetE = (180/3.1415)*atan2(sqtr(1-D)D )
+targetS = (180/3.1415)*(atan2(targetY/targetX) - atan2((L2*sin(targetE))/(L1+cos(targetE))))
+errorE = TargetE - elbowEncoder
+errorS = TargetS - shoulderEncoder
 
-targetE = (180/pi)*targetE
-
- printf("the tagets are x y E and S %d %d %d %d \n" targetX, targetY, targetE, targetS)
+ printf("the targets are x y E and S %d %d %d %d \n" targetX, targetY, targetE, targetS)
 // use targetE and targetS to calculate errorE and errorS using encoder date
-// multiply kPE*errrorE and kPS*errorS to find motor outputs
+// multiply kPE*errorE and kPS*errorS to find motor outputs
 //set motor power = to outputs
 
 
